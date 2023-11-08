004f55e34ae041c87d7dbef280cb793b15313533
Project created <eng.erickwesley@gmail.com>
2023-11-08 08:13:47 -0300

    initial commit

---
+++ src/main/java/tumnus/landpage/LandpageApplication.java
@@ -0,0 +1,13 @@
+package tumnus.landpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tumnus/landpage/LandpageApplicationTests.java
@@ -0,0 +1,13 @@
+package tumnus.landpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
